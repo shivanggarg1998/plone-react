1930244eda3181a7d3290ae9f9aab5ca4b327161
nileshgulia1 <nileshgulia@gmail.com>
2018-06-17 18:25:01 +0530

    removed browsers package

--- scripts/init.js
+++ scripts/init.js
@@ -11,7 +11,6 @@ const path = require('path');
 const chalk = require('chalk');
 const execSync = require('child_process').execSync;
 const spawn = require('react-dev-utils/crossSpawn');
-const { defaultBrowsers } = require('react-dev-utils/browsersHelper');
 const os = require('os');
 
 function isInGitRepository() {
